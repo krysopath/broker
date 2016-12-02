51ad87ac6e30f658bb8aa075dd2683e22859c5d9
Georg vom Endt <krysopath@gmail.com>
2016-12-02 10:17:28 +0100

    first commit

--- broker/broker.py
+++ broker/broker.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python3
 # coding=utf-8
-from flask import Flask, jsonify
+from flask import Flask, jsonify, g
 from flask_restful import Resource, Api
 from interactor import Interactor, FuelModel, Fuel
 
 app = Flask(__name__)
 api = Api(app)
-i = Interactor('fuel', FuelModel, Fuel)
+
+
+def get_interactor():
+    iactor = getattr(g, 'iactor', None)
+    if iactor is None:
+        iactor = g._database = Interactor('fuel', FuelModel, Fuel)
+    return iactor
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    iactor = getattr(g, 'iactor', None)
+    if iactor is not None:
+        iactor.close()
 
 
 class FueLRessoure(Resource):
+    #@app.cli.command('get_interactor')
     def get(self, _id):
-        return jsonify(i.get_data(_id))
+        return jsonify(g.iactor.get_data(_id))
 
 api.add_resource(FueLRessoure, '/fuel=<int:_id>')
 
