9b174d580e56796421e15b99fb8ff3eca8efe18f
Georg vom Endt <krysopath@gmail.com>
2016-12-02 09:56:21 +0100

    first commit

--- broker/broker.py
+++ broker/broker.py
@@ -2,15 +2,18 @@
 # coding=utf-8
 from flask import Flask
 from flask_restful import Resource, Api
+from interactor import Interactor, FuelModel, Fuel
 
 app = Flask(__name__)
 api = Api(app)
+i = Interactor('fuel', FuelModel, Fuel)
 
-class HelloWorld(Resource):
+
+class FueLRessoure(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(FueLRessoure, '/')
 
 if __name__ == '__main__':
     app.run(debug=True, port=4444, host='0.0.0.0')
