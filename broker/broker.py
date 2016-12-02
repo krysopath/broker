1d006956ca342e38a49861ca004a0a4d53580a7e
Georg vom Endt <krysopath@gmail.com>
2016-12-02 11:32:57 +0100

    first commit

--- broker/broker.py
+++ broker/broker.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python3
 # coding=utf-8
-from flask import Flask, jsonify, g, request
+from flask import Flask, jsonify, g, request, make_response
 from flask_restful import Resource, Api
 from interactor import Interactor, FuelModel, Fuel
+from flask.ext.httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
 api = Api(app)
+auth = HTTPBasicAuth()
+
+
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 def get_interactor():
@@ -13,6 +32,7 @@ def get_interactor():
     if iactor is None:
         iactor = g._iactor = Interactor('fuel', FuelModel, Fuel)
     return iactor
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
@@ -23,9 +43,12 @@ def teardown_db(exception):
 
 class FueLRessoure(Resource):
     fuels = {'1': {'data': 'blabla'}}
+
+    @auth.login_required
     def get(self, fuel_id):
         return jsonify({fuel_id: self.fuels[fuel_id]})
 
+    @auth.login_required
     def put(self, fuel_id):
         self.fuels[fuel_id] = request.form['data']
         return jsonify({fuel_id: self.fuels[fuel_id]})
