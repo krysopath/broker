ced4a3fa8d897f156321c349dac02740fc67e5f3
Georg vom Endt <krysopath@gmail.com>
2016-12-02 11:33:40 +0100

    first commit

--- broker/broker.py
+++ broker/broker.py
@@ -12,8 +12,8 @@ auth = HTTPBasicAuth()
 
 @auth.get_password
 def get_password(username):
-    if username == 'miguel':
-        return 'python'
+    if username == 'g':
+        return 'g25v09e85'
     return None
 
 
