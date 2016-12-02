589fad3510f9a43b8593793ce6f06e3d7cafb67e
Georg vom Endt <krysopath@gmail.com>
2016-12-02 11:33:55 +0100

    first commit

--- broker/broker.py
+++ broker/broker.py
@@ -56,4 +56,4 @@ class FueLRessoure(Resource):
 api.add_resource(FueLRessoure, '/fuel/<string:fuel_id>')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=4444, host='0.0.0.0')
+    app.run(debug=False, port=4444, host='0.0.0.0')
